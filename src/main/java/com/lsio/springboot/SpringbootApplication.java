6bc28cddcb037795a0c33b62af47470b6b9a0aac
MonsieurHao <totocosta29@hotmail.fr>
2023-11-02 16:19:19 +0100

    Microservices operational

--- src/main/java/com/lsio/springboot/SpringbootApplication.java
+++ src/main/java/com/lsio/springboot/SpringbootApplication.java
@@ -2,11 +2,13 @@ package com.lsio.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class})
 @EnableJpaRepositories
+@EnableFeignClients
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
